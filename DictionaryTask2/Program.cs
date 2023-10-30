379111ecf64e9bd5e66ed82e1fcaea0522dd97ee
Elitsa Georgeva <Elitsa Georgeva@DESKTOP-8D1ST4F>
2023-10-30 14:18:56 +0200

    Add project files.

---
+++ DictionaryTask2/Program.cs
@@ -0,0 +1,41 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // Create dictionaries representing courses and their credit hours
+        Dictionary<string, int> courseCredits = new Dictionary<string, int>();
+
+
+        courseCredits.Add("Physics", 6);
+        courseCredits.Add("Math", 8);
+        courseCredits.Add("Informatics", 5);
+        courseCredits.Add("English", 3);
+
+        Dictionary<string, List<string>> studentCourses = new Dictionary<string, List<string>>();
+
+        studentCourses.Add("Stan", new List<string> { "Math", "English" });
+        studentCourses.Add("Eli", new List<string> { "Math", "Physics" });
+        studentCourses.Add("Tori", new List<string> { "Informatics", "English" });
+
+        Dictionary<string, int> totalCreds = new Dictionary<string, int>();
+
+        foreach (var s in studentCourses.Keys)
+        {
+
+            int count = 0;
+            foreach (var c in studentCourses[s])
+            {
+                if (courseCredits.ContainsKey(c))
+                {
+                    count += courseCredits[c];
+                }
+            }
+            totalCreds[s] = count;
+        }
+
+        foreach (var s in totalCreds)
+        {
+            Console.WriteLine($"{s.Key}, {s.Value} credit hours");
+        }
+    }
+}
